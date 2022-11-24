e3d4ffc1a1ab1604172a11d76e1c2a8587e20db9
ek178 <elad.kreisel@mail.huji.ac.il>
2022-11-24 18:26:39 +0200

    navbar-final try

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ api = Flask(__name__)
 
 @api.route('/')
 def index():
-    nav= [{"name": "Home", "url": "https://example.com/1"},
-        {"name": "About", "url": "https://example.com/2"},
-        {"name": "Pics", "url": "https://example.com/3"},
-    ]
     return render_template (
         "index.html")
  
@@ -21,9 +17,9 @@ def index():
 def name():
     return render_template ("data1.html")
 
-#@api.route('/data2')
-#def age():
-#    return render_template ("data2.html")
+@api.route('/data2')
+def age():
+    return render_template ("data2.html")
 
 
 if __name__ == '__main__':
