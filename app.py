ce7286c7d1beedfa8a52392c0e46171a2b900b7b
ek178 <elad.kreisel@mail.huji.ac.il>
2022-11-24 18:17:29 +0200

    navbar-final try

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-import flask
-import jinja_partials
-from views import home
+from flask import Flask,render_template
+from flask import current_app as api
+#import jinja_partials
 
-api = flask.Flask(__name__)
-api.register_blueprint(home.blueprint)
-jinja_partials.register_extensions(api)
+api = Flask(__name__)
+#api.register_blueprint(home.blueprint)
+#jinja_partials.register_extensions(api)
 
 
 
-#@api.route('/')
-#def index():
-#    return render_template ("templates/index.html")
+@api.route('/')
+def index():
+    nav= [{"name": "Home", "url": "https://example.com/1"},
+        {"name": "About", "url": "https://example.com/2"},
+        {"name": "Pics", "url": "https://example.com/3"},
+    ]
+    return render_template (
+        "index.html")
  
-#@api.route('/data1')
-#def name():
-#    return render_template ("data1.html")
+@api.route('/data1')
+def name():
+    return render_template ("data1.html")
 
 #@api.route('/data2')
 #def age():
--- views/home.py
+++
@@ -1,8 +0,0 @@
-import flask
-
-blueprint = flask.Blueprint('home', __name__, template_folder='templates')
-
-
-@blueprint.get('/')
-def index():
-    return flask.render_template('home/index.html')
